ff09b54a3dba4de3e76616626295e17a8dc3058e
pippokill <pierpaolo.basile@gmail.com>
2026-03-22 19:53:23 +0100

    fix esercitazione 3

--- esercitazioni/es_2_1.c
+++ esercitazioni/es_2_1.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 #include <time.h>
 #include <stdlib.h>
+#include <string.h>
 
-#define STUDENTI 2
+#define STUDENTI 10
 #define ESAMI 20
 #define STRING_LENGHT 40
-#define NOMI_ESAMI 5
+#define NOMI_ESAMI 20
+#define MAX_NOMI 8
 
 typedef struct
 {
@@ -34,75 +36,35 @@ int main()
     studente classe[STUDENTI] = {"", "", {0, 0, 0}, 0, {"", 0}};
     int seed = time(NULL);
     static int giorni_validi[12] = {31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-    char nomiEsami[NOMI_ESAMI][STRING_LENGHT] = {"Programmazione", "Logica", "Analisi matematica", "Matematica discreta", "Ingegneria del software"};
+    char nomiEsami[NOMI_ESAMI][STRING_LENGHT] = {"Programmazione", "Logica", "Analisi matematica", "Matematica discreta", "Ingegneria del software", "Ingegneria della conoscenza", "Calcolo numerico", "Geometria", "Fisica", "Interazione Uomo-Macchina", "Sicurezza nelle applicazioni", "Analisi II", "Apprendimento automatico", "Inglese", "Algebra", "Sistemi operativi", "Basi di dati", "Architettura degli elaboratori", "Reti", "Sistemi Informativi"};
+    char nomi[MAX_NOMI][STRING_LENGHT] = {"Marco", "Chiara", "Francesca", "Luca", "Lucia", "Francesco", "Laura", "Maria"};
+    char cognomi[MAX_NOMI][STRING_LENGHT] = {"Rossi", "Bianchi", "Verdi", "Esposito", "Ferrari", "Greco", "Ricci", "Colombo"};
     srand(seed);
+    printf("\n === STUDENTI ===\n");
     for (unsigned int i = 0; i < STUDENTI; i++)
     {
-        printf("Inserire nome studente %d: ", i + 1);
-        scanf("%s", classe[i].nome);
-        printf("Inserire cognome studente %d: ", i + 1);
-        scanf("%s", classe[i].cognome);
-        do
-        {
-            printf("Inserire matricola studente %d: ", i + 1);
-            scanf("%d", &classe[i].matricola);
-            if (classe[i].matricola <= 0)
-            {
-                printf("\nLa matricola non è valida, deve essre maggiore di 0!\n");
-            }
-        } while (classe[i].matricola <= 0);
-        do
-        {
-            printf("Inserire mese di nascita studente %d: ", i + 1);
-            scanf("%d", &classe[i].nascita.mese);
-            if (classe[i].nascita.mese < 1 || classe[i].nascita.mese > 12)
-            {
-                printf("\nIl mese di nascita non è valido, deve essre compreso tra 1 e 12!\n");
-            }
-        } while (classe[i].nascita.mese < 1 || classe[i].nascita.mese > 12);
-        do
-        {
-            printf("Inserire giorno di nascita studente %d: ", i + 1);
-            scanf("%d", &classe[i].nascita.giorno);
-            if (classe[i].nascita.giorno < 1 || classe[i].nascita.giorno > giorni_validi[classe[i].nascita.mese - 1])
-            {
-                printf("\nIl giorno di nascita non è valido per il mese selezionato, deve essre compreso tra 1 e %d!\n", giorni_validi[classe[i].nascita.mese - 1]);
-            }
-        } while (classe[i].nascita.giorno < 1 || classe[i].nascita.giorno > giorni_validi[classe[i].nascita.mese - 1]);
-        do
-        {
-            printf("Inserire anno di nascita studente %d: ", i + 1);
-            scanf("%d", &classe[i].nascita.anno);
-            if (classe[i].nascita.anno < 1800)
-            {
-                printf("\nL'anno di nascita non è valido, deve essre maggiore di 1799!\n");
-            }
-        } while (classe[i].nascita.anno < 1800);
+        strcpy(classe[i].nome, nomi[rand() % MAX_NOMI]);
+        strcpy(classe[i].cognome, cognomi[rand() % MAX_NOMI]);
+        // potrei generara due matricole uguali per due studenti diversi
+        classe[i].matricola = rand() % (70000 - 999) + 999;
+        printf("\tStudente: %s %s, matricola: %d\n", classe[i].cognome, classe[i].nome, classe[i].matricola);
+        // generazione data
+        classe[i].nascita.anno = rand() % (2008 - 1950) + 1950;
+        classe[i].nascita.mese = rand() % 12 + 1;
+        classe[i].nascita.giorno = rand() % giorni_validi[classe[i].nascita.mese - 1] + 1;
+        printf("\tData di nascita: %d/%d/%d\n", classe[i].nascita.giorno,classe[i].nascita.mese, classe[i].nascita.anno);
+        printf("\t=== LIBRETTO STUDENTE ===\n");
         for (unsigned int j = 0; j < ESAMI; j++)
         {
             classe[i].libretto[j].voto = rand() % (31 - 18 + 1) + 18;
-            int esami_rand[NOMI_ESAMI] = {0};
-            int ke = 0;
-            for (ke = 0; ke < NOMI_ESAMI; ke++)
-            {
-                esami_rand[ke] = rand() % NOMI_ESAMI;
-                for (int ch = 0; ch < ke; ch++)
-                {
-                    if (esami_rand[ke]==esami_rand[ch])
-                    {
-                        ke--;
-                        break;
-                    }
-                }
-            }
-            for (ke = 0; ke < NOMI_ESAMI; ke++)
-            {
-                for (int k = 0; k < STRING_LENGHT; k++)
-                    classe[i].libretto[j].insegnamento[k] = nomiEsami[esami_rand[ke]][k];
-            }
+            // potrei inserire due volte lo stesso esame
+            int ei = rand() % NOMI_ESAMI;
+            strcpy(classe[i].libretto[j].insegnamento, nomiEsami[ei]);
+            printf("\t\t%s, voto: %d\n", classe[i].libretto[j].insegnamento, classe[i].libretto[j].voto);
         }
+        printf("\t=========================================\n");
     }
-    printf("\n === MEDIA ESAMI PER STUDENTE ===\n");
+    printf("\n=== MEDIA ESAMI PER STUDENTE ===\n");
     // calcola media
     int max_pos = -1;
     float max_avg = 0;
@@ -134,14 +96,14 @@ int main()
             max_pos = i;
         }
         printf("Media esami studente %s %s: %.2f\n", classe[i].cognome, classe[i].nome, avg);
-        printf("Esame con il voto più basso: %s\t%d\n", classe[i].libretto[s_min_pos].insegnamento, classe[i].libretto[s_min_pos].voto);
-        printf("Esame con il voto più alto: %s\t%d\n", classe[i].libretto[s_max_pos].insegnamento, classe[i].libretto[s_max_pos].voto);
+        printf("Esame con il voto più basso: %s %d\n", classe[i].libretto[s_min_pos].insegnamento, classe[i].libretto[s_min_pos].voto);
+        printf("Esame con il voto più alto: %s %d\n\n", classe[i].libretto[s_max_pos].insegnamento, classe[i].libretto[s_max_pos].voto);
     }
 
     // stampa nome e cognome dello studente che ha la media più alta
     if (max_pos > -1)
     {
-        printf("\nStudente con la media più alta: %s\t%s\n", classe[max_pos].nome, classe[max_pos].cognome);
+        printf("\nStudente con la media più alta: %s %s\n", classe[max_pos].nome, classe[max_pos].cognome);
     }
 
     int freq[14] = {0};
